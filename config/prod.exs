0f8e3f007a7654ac27f782fb4cf394b238b1742f
Ian Mortel <noel_delcastillo@medilink.com.ph>
2017-01-18 14:55:02 +0800

    Edited the URL in prod.ex

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
   config :rumbl, Rumbl.Endpoint,
 http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "boiling-everglades-86969.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "ian-mortel-gforce.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
